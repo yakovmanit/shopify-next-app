2e1f0434944c3b3f4ed2738aa207a19b277082ee
yakiv <yakov.manitskiy@gmail.com>
2025-11-27 18:39:10 +0200

    chore: add new packages, add config file for graphQL typing
    - add admin-api-client,storefront-api-client, react-slick, slick-carousel, api-codegen-preset, concurrently
    - update start project command 'dev' with graphql-codegen watching
    - update eslint version
    - update gitignore

---
+++ .graphqlrc.ts
@@ -0,0 +1,31 @@
+import { shopifyApiProject, ApiType } from '@shopify/api-codegen-preset';
+
+export default {
+  schema: 'https://shopify.dev/storefront-graphql-direct-proxy/2025-10',
+  documents: ['./src/**/*.{js,ts,jsx,tsx}'],
+  projects: {
+    // Default Storefront API
+    default: shopifyApiProject({
+      apiType: ApiType.Storefront,
+      apiVersion: '2025-10',
+      documents: [
+        './src/app/**/*.{js,ts,jsx,tsx}',
+        './src/components/**/*.{js,ts,jsx,tsx}',
+        './src/lib/shopify/**/*.{js,ts}',
+        './src/services/**/*.{js,ts}',
+      ],
+      outputDir: './src/types/storefront',
+    }),
+
+    // Admin API
+    admin: shopifyApiProject({
+      apiType: ApiType.Admin,
+      apiVersion: '2025-10',
+      documents: [
+        './src/app/api/**/*.{js,ts}',
+        './src/lib/shopify/admin/**/*.{js,ts}',
+      ],
+      outputDir: './src/types/admin',
+    }),
+  },
+};
