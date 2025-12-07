1530e884a0047034373f8ed4b2df61c4c21cb704
yakiv <yakov.manitskiy@gmail.com>
2025-12-07 21:27:11 +0200

    refactor(Auth): replace RegisterForm to a separate file

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -2,12 +2,7 @@
 
 import React, { useState } from "react";
 import { User, X } from "lucide-react";
-import {useForm} from "react-hook-form";
-import {zodResolver} from "@hookform/resolvers/zod";
-import {sighUpSchema} from "@/lib";
-import {TSignUpSchema} from "@/types";
-import {createCustomer} from "@/services";
-import toast from "react-hot-toast";
+import {RegisterForm} from "./RegisterForm";
 
 interface Props {
   className?: string;
@@ -21,28 +16,6 @@ export const Auth: React.FC<Props> = ({ className }) => {
   const handleCloseAuthPopup = () => {
     setIsAuthPopupOpen(false);
     setTimeout(() => setAuthMode("login"), 300);
-  };
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-    reset,
-  } = useForm<TSignUpSchema>({
-    resolver: zodResolver(sighUpSchema)
-  });
-
-  const onSubmit = async (data: TSignUpSchema) => {
-    const createCustomerRes = await createCustomer(data.email, data.password, data.firstName, data.lastName);
-
-    if (createCustomerRes) {
-      toast.success('Account created successfully!');
-    } else {
-      toast.error('Failed to create account. Please try again.');
-    }
-
-    setIsAuthPopupOpen(false);
-    reset();
   };
 
   return (
@@ -121,83 +94,10 @@ export const Auth: React.FC<Props> = ({ className }) => {
               </form>
             ) : (
               /* Register form */
-              <form
-                onSubmit={handleSubmit(onSubmit)}
-                className="space-y-5"
-              >
-                <div>
-                  <label className="block text-sm text-gray-700 mb-1">First Name</label>
-                  <input
-                    {
-                      ...register("firstName")
-                    }
-                    type="text"
-                    className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:ring-2 focus:ring-black outline-none"
-                    placeholder="John"
-                  />
-                </div>
-
-                <div>
-                  <label className="block text-sm text-gray-700 mb-1">Last Name</label>
-                  <input
-                    {
-                      ...register("lastName")
-                    }
-                    type="text"
-                    className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:ring-2 focus:ring-black outline-none"
-                    placeholder="Doe"
-                  />
-                </div>
-
-                <div>
-                  <label className="block text-sm text-gray-700 mb-1">Email</label>
-                  <input
-                    {...register("email", {
-                      required: "Email is required",
-                    })}
-                    type="email"
-                    className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:ring-2 focus:ring-black outline-none"
-                    placeholder="example@gmail.com"
-                  />
-                  {errors.email && (
-                    <p className="text-red-500 text-sm pt-1">{errors.email.message}</p>
-                  )}
-                </div>
-
-                <div>
-                  <label className="block text-sm text-gray-700 mb-1">Password</label>
-                  <input
-                    {...register("password", {
-                      required: "Password is required",
-                    })}
-                    type="password"
-                    className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:ring-2 focus:ring-black outline-none"
-                    placeholder="Create a password"
-                  />
-                  {errors.password && (
-                    <p className="text-red-500 text-sm pt-1">{errors.password.message}</p>
-                  )}
-                </div>
-
-                <button
-                  type="submit"
-                  className="w-full py-2 bg-black text-white rounded-lg font-semibold hover:bg-gray-800 transition"
-                >
-                  Create Account
-                </button>
-
-                <p className="text-center text-sm text-gray-600 mt-4">
-                  Already have an account?{" "}
-                  <button
-                    disabled={isSubmitting}
-                    type="button"
-                    className="text-black font-medium hover:underline"
-                    onClick={() => setAuthMode("login")}
-                  >
-                    Sign In
-                  </button>
-                </p>
-              </form>
+              <RegisterForm
+                closeAuthPopup={handleCloseAuthPopup}
+                setAuthMode={setAuthMode}
+              />
             )}
           </div>
         </div>
