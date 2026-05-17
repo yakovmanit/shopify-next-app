a169797a445122f0606e141248574c4e782820f6
yakiv <yakov.manitskiy@gmail.com>
2026-05-18 02:11:44 +0300

    chore: implement Customer Account API types generation with codegen
    - install hydrogen-codegen package
    - install api-codegen-preset

--- .graphqlrc.ts
+++ .graphqlrc.ts
@@ -1,5 +1,7 @@
 import { shopifyApiProject, ApiType } from '@shopify/api-codegen-preset';
+import { preset as hydrogenPreset, pluckConfig } from '@shopify/hydrogen-codegen';
 
+// TODO: migrate Storefront API from proxy to @shopify/hydrogen-codegen
 export default {
   schema: 'https://shopify.dev/storefront-graphql-direct-proxy/2025-10',
   documents: ['./src/**/*.{js,ts,jsx,tsx}'],
@@ -29,5 +31,19 @@ export default {
       ],
       outputDir: './src/types/admin',
     }),
+
+    // Customer Account API - hydrogen-codegen
+    customer: {
+      schema: './schemas/customer-account.schema.json',
+      documents: ['./src/constantsAccount/**/*.{js,ts}'],
+      extensions: {
+        codegen: {
+          pluckConfig,
+          generates: {
+            'src/types/customer/customer.generated.d.ts': { preset: hydrogenPreset },
+          },
+        },
+      },
+    },
   },
 };
